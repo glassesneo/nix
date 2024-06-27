868aa6574b7658aee1d419c16a33acf523d400b6
glassesneo <glassesneo@protonmail.com>
2024-06-28 02:35:33 +0900

    :bug: Fix nix-on-droid options

--- system/android/configuration.nix
+++ system/android/configuration.nix
@@ -4,24 +4,10 @@ let
   hostname = "localhost";
 in
   {
-    imports = [
-      ../common.nix
-    ];
+    system.stateVersion = "23.11";
 
-    nix = {
-      settings = {
-        # substituers that will be considered before the official ones(https://cache.nixos.org)
-        substituters = [
-          "https://mirror.sjtu.edu.cn/nix-channels/store"
-          "https://nix-community.cachix.org"
-        ];
-        trusted-users = [ username ];
-        trusted-public-keys = [
-          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        ];
-        builders-use-substitutes = true;
-      };
-    };
-
+    nix.extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
     time.timeZone = "Japan/Tokyo";
   }
