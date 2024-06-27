32be78ea0627b32abf4c686e8b15d146f5a8ef69
glassesneo <glassesneo@protonmail.com>
2024-06-28 02:31:24 +0900

    :bug: Fix nix-on-droid option

--- system/android/configuration.nix
+++ system/android/configuration.nix
@@ -21,11 +21,6 @@ in
         ];
         builders-use-substitutes = true;
       };
-      gc = {
-        automatic = true;
-        interval = { Weekday = 0; Hour = 0; Minute = 0; };
-        options = "--delete-older-than 7d";
-      };
     };
 
     time.timeZone = "Japan/Tokyo";
